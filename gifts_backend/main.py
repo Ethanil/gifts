e601ee8f284dfa1b7744e4841226dfeee33a121a
Ethanil <69218171+Ethanil@users.noreply.github.com>
2024-02-19 20:26:10 +0100

    fixed main.py

--- gifts_backend/main.py
+++ gifts_backend/main.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 from os import getenv
 import models
 
-
+app = config.connex_app
+app.add_api(config.basedir / "openapi.yaml")
+with app.app.app_context():
+    config.db.create_all()
 
 if __name__ == "__main__":
     load_dotenv()
-    app = config.connex_app
-    app.add_api(config.basedir / "openapi.yaml")
-    with app.app.app_context():
-        config.db.create_all()
     app.run(host=getenv('BACKEND_HOST'), port=int(getenv('BACKEND_PORT')))
